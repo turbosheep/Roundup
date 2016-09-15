78b827ccc413a09149857101947d3599057bd22c
turbosheep <hannahreyre@gmail.com>
2016-09-15 02:18:47 -0600

    started the boids, react to click, but only in one direction

---
+++ Roundup/Assets/Scripts/Boid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boid : MonoBehaviour {
+
+    private Rigidbody2D body;
+    private RaycastHit2D hit;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2(Random.value, Random.value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            //hit = Physics2D.Raycast(Input.mousePosition, this.transform.position);
+            Vector2 v = Input.mousePosition - this.transform.position;
+
+            body.AddForce(v);
+            Debug.Log(body.velocity);
+        }
+	}
+}
---
+++ Roundup/Assets/Scripts/BoidManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidManager : MonoBehaviour {
+
+    public GameObject boid;
+    public int numBoids = 3;
+
+    public GameObject[] flock;
+
+    public Vector2 flockCenter;
+    public Vector2 flockVelocity;
+
+	// Use this for initialization
+	void Start () {
+        GameObject parent = new GameObject();
+        parent.name = "flock";
+        flock = new GameObject[numBoids];
+	    for(int i = 0; i < numBoids; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-10f, 10f), Random.Range(-5.0f, 5.0f));
+            GameObject b = Instantiate(boid, pos, Quaternion.identity, parent.transform) as GameObject;
+            flock[i] = b;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 c = Vector3.zero;
+        Vector2 v = Vector2.zero;
+
+        foreach(GameObject boid in flock)
+        {
+            c += boid.transform.localPosition;
+            v += boid.GetComponent<Rigidbody2D>().velocity;
+        }
+
+        flockCenter = c / numBoids;
+        flockVelocity = v / numBoids;
+	
+	}
+}
