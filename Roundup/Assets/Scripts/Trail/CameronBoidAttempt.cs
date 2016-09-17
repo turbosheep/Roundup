469fe991ba6fb0e1297d8f7db2a5a2a235d61b62
kamaros <kamaros173@gmail.com>
2016-09-17 15:48:28 -0600

    Experimenting with tracing a path.  Added simple functionality to my personal boids to not leave the screen.

--- Roundup/Assets/Scripts/Trail/CameronBoidAttempt.cs
+++ Roundup/Assets/Scripts/Trail/CameronBoidAttempt.cs
@@ -3,10 +3,9 @@ using System.Collections;
 
 public class CameronBoidAttempt : MonoBehaviour {
 
-
     public float StartRepelRange = 1;
     public float RatioSpeedMultiplier = 0.5f;
-
+    public float EdgeTol = 0;
 
     private GameObject dog;
     private Vector3 startPos;
@@ -27,14 +26,31 @@ public class CameronBoidAttempt : MonoBehaviour {
 
             if(StartRepelRange - dist > 0)
             {
-                float speed = 1 + (dist / RatioSpeedMultiplier);
+                float speed = 1 + (RatioSpeedMultiplier / dist);
                 Vector2 direction = transform.position - dog.transform.position;
-                direction = direction.normalized;
+                
+                Vector3 edge = Camera.main.WorldToViewportPoint(transform.position);
+                if (edge.x < (0.0 + EdgeTol) || edge.x > (1.0 - EdgeTol))
+                {
+                    direction.x = -direction.x;
+                }
+                if (edge.y < (0.0 + EdgeTol) || edge.y > (1.0 - EdgeTol))
+                {
+                    direction.y = -direction.y;
+                }
 
+                direction = direction.normalized;
                 transform.position = Vector2.Lerp(transform.position, (Vector2)transform.position + direction, speed * Time.deltaTime);
 
             }
-
+            else
+            {
+                transform.position = Vector2.Lerp(transform.position, Vector2.zero, 1 * Time.deltaTime);
+            }
         }
-	}
+        else
+        {
+            transform.position = Vector2.Lerp(transform.position, Vector2.zero, 1 * Time.deltaTime);
+        }
+    }
 }
