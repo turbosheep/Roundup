4e9935f6f007a8631777494dbb6c252ee38f3b98
ImpDesign <impdesign.bm@gmail.com>
2016-09-16 20:47:11 -0600

    Attact and Repel objects
    See Brady Test scene. If you have your mouse on the screen it will repel
    the sprite. If you press the left mouse button it will attact the
    sprite.

---
+++ Roundup/Assets/Scripts/Motion.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motion : MonoBehaviour
+{
+    public float speed = 3;
+    protected Rect screenRect;
+
+	// Used for initialization
+	void Start ()
+    {
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+    }
+	
+    //Moves obj away from the mouse
+	public void Repel(Transform obj)
+    {
+        if(screenRect.Contains(Input.mousePosition))
+        {
+            Rotate(obj);
+            obj.position -= obj.up * speed * Time.deltaTime;
+        }
+    }
+
+    //Moves obj towards the mouse
+    public void Attract(Transform obj)
+    {
+        if (screenRect.Contains(Input.mousePosition))
+        {
+            Rotate(obj);
+            obj.position += obj.up * speed * Time.deltaTime;
+        }
+    }
+
+    //Gets the mouse position
+    protected Vector3 GetMousePos()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = new Vector3(mousePos.x, mousePos.y, 0);
+        return mousePos;
+    }
+
+    //Rotates the transform to the mouse point
+    protected void Rotate(Transform obj)
+    {
+        Vector3 mousePos = GetMousePos();
+        Vector3 diff = mousePos - obj.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
+}
