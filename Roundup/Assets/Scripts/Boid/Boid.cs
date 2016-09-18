dcffa8fef5c78eb0ccf0471bd05951bef4fa0a10
turbosheep <hannahreyre@gmail.com>
2016-09-18 16:27:04 -0600

    new version of boids, creating new branch to not interfere with other work

--- Roundup/Assets/Scripts/Boid/Boid.cs
+++ Roundup/Assets/Scripts/Boid/Boid.cs
@@ -1,26 +1,175 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Boid : MonoBehaviour {
+public class Boid : MonoBehaviour
+{
 
-    private Rigidbody2D body;
-    private RaycastHit2D hit;
+	public float separation = 2.5f;
+	public float neighborRadius = 5.0f;
+
+	public float separationWeight = 1f;
+	public float alignmentWeight = 1f;
+	public float cohesionWeight = 1f;
+
+	private Vector2 location;
+	private Vector2 velocity;
+	private Vector2 acceleration;
+
+	public Boid[] boids { get; set; }
+
+	public float mForce = 1f;
+	public float mSpeed = 1f;
 
 	// Use this for initialization
-	void Start () {
-        body = GetComponent<Rigidbody2D>();
-        body.velocity = new Vector2(Random.value, Random.value);
+	void Start()
+	{
+		velocity = new Vector2();
+		acceleration = new Vector2();
+		location = this.gameObject.transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if(Input.GetMouseButtonDown(0))
-        {
-            //hit = Physics2D.Raycast(Input.mousePosition, this.transform.position);
-            Vector2 v = Input.mousePosition - this.transform.position;
 
-            body.AddForce(v);
-            Debug.Log(body.velocity);
-        }
+	// Update is called once per frame
+	void Update()
+	{
+		if (boids != null)
+		{
+			flock();
+			velocity += acceleration;
+			velocity = Vector2.ClampMagnitude(velocity, mSpeed);
+			location += velocity;
+			acceleration = Vector2.zero;
+			this.gameObject.transform.position = location;
+		}
 	}
+
+	private void flock()
+	{
+
+		//find the three components of the flock
+		Vector2 sep = Separate();
+		Vector2 ali = Align();
+		Vector2 coh = Cohesion();
+
+		//weight them
+		sep *= separationWeight;
+		ali *= alignmentWeight;
+		coh *= cohesionWeight;
+
+		//apply to the boid
+		ApplyForce(sep);
+		ApplyForce(ali);
+		ApplyForce(coh);
+	}
+
+	public void ApplyForce(Vector2 force)
+	{
+		acceleration += force;
+	}
+
+	/// <summary>
+	/// Calculate the force applied to steer a boid towards a target.
+	/// </summary>
+	/// <param name="target"></param>				
+	/// <returns></returns>
+	private Vector2 Seek(Vector2 target)
+	{
+		//get a vector to the desired location, capped for speed
+		Vector2 desired = location - target;
+		desired.Normalize();
+		if (desired.magnitude > mSpeed)
+			desired = Vector2.ClampMagnitude(desired, mSpeed);
+
+		//get the seering (desired location - velocity), capped for force
+		Vector2 steer = desired - velocity;
+		if (steer.magnitude > mForce)
+			steer = Vector2.ClampMagnitude(steer, mForce);
+
+		//return the steering vector
+		return steer;
+	}
+
+	private Vector2 Separate()
+	{
+		Vector2 steer = new Vector2();
+		int count = 0;
+
+		foreach (Boid b in boids)
+		{
+			Vector2 dist = location - b.location;
+			float d = dist.magnitude;
+			if (d > 0 && d < separation)
+			{
+				dist.Normalize();
+				dist /= d;
+				steer += dist;
+				count++;
+			}
+			if (count > 0)
+			{
+				steer /= (float)count;
+			}
+			if (steer.magnitude > 0)
+			{
+				steer.Normalize();
+				steer *= mSpeed;
+				steer = steer - velocity;
+				if (steer.magnitude > mForce)
+					steer = Vector2.ClampMagnitude(steer, mForce);
+				return steer;
+			}
+		}
+		Debug.Log("Separation 0");
+		return new Vector2();
+	}
+
+	private Vector2 Align()
+	{
+		Vector2 sum = new Vector2();
+		int count = 0;
+		foreach (Boid b in boids)
+		{
+			float d = (location - b.location).magnitude;
+			if (d > 0 && d < neighborRadius)
+			{
+				sum += b.velocity;
+				count++;
+			}
+		}
+		if (count > 0)
+		{
+			sum /= (float)count;
+			sum = Vector2.ClampMagnitude(sum, mSpeed);
+
+			Vector2 steer = sum - velocity;
+			if (steer.magnitude > mForce)
+				steer = Vector2.ClampMagnitude(steer, mForce);
+			return steer;
+		}
+		Debug.Log("Align 0");
+		return new Vector2();
+	}
+
+	private Vector2 Cohesion()
+	{
+		Vector2 sum = new Vector2();
+		int count = 0;
+
+		foreach (Boid b in boids)
+		{
+			float d = (location - b.location).magnitude;
+			if (d > 0 && d < neighborRadius)
+			{
+				sum += b.location;
+				count++;
+			}
+		}
+		if (count > 0)
+		{
+			sum /= (float)count;
+			return Seek(sum);
+		}
+		Debug.Log("Cohesion 0");
+		return new Vector2();
+	}
+
 }
--- Roundup/Assets/Scripts/Boid/BoidManager.cs
+++ Roundup/Assets/Scripts/Boid/BoidManager.cs
@@ -1,42 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BoidManager : MonoBehaviour {
+public class BoidManager : MonoBehaviour
+{
 
-    public GameObject boid;
-    public int numBoids = 3;
+	public GameObject boid;
+	public int numBoids = 3;
 
-    public GameObject[] flock;
+	public Boid[] flock;
 
-    public Vector2 flockCenter;
-    public Vector2 flockVelocity;
+	public Vector2 flockCenter;
+	public Vector2 flockVelocity;
 
 	// Use this for initialization
-	void Start () {
-        GameObject parent = new GameObject();
-        parent.name = "flock";
-        flock = new GameObject[numBoids];
-	    for(int i = 0; i < numBoids; i++)
-        {
-            Vector2 pos = new Vector2(Random.Range(-10f, 10f), Random.Range(-5.0f, 5.0f));
-            GameObject b = Instantiate(boid, pos, Quaternion.identity, parent.transform) as GameObject;
-            flock[i] = b;
-        }
+	void Awake()
+	{
+		flock = new Boid[numBoids];
+		for (int i = 0; i < numBoids; i++)
+		{
+			Vector2 pos = new Vector2(Random.Range(-10f, 10f), Random.Range(-5.0f, 5.0f));
+			//GameObject temp = Instantiate(boid, pos, Quaternion.identity, this.transform) as GameObject;
+			GameObject temp = Instantiate(boid, new Vector2(), Quaternion.identity, this.transform) as GameObject;
+			Boid b = temp.GetComponent<Boid>();
+			flock[i] = b;
+		}
+		foreach (Boid b in flock)
+		{
+			b.boids = flock;
+		}
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-        Vector3 c = Vector3.zero;
-        Vector2 v = Vector2.zero;
+	void Update()
+	{
+		Vector3 c = Vector3.zero;
+		Vector2 v = Vector2.zero;
 
-        foreach(GameObject boid in flock)
-        {
-            c += boid.transform.localPosition;
-            v += boid.GetComponent<Rigidbody2D>().velocity;
-        }
+		foreach (Boid boid in flock)
+		{
+			c += boid.transform.localPosition;
+			v += boid.GetComponent<Rigidbody2D>().velocity;
+		}
 
-        flockCenter = c / numBoids;
-        flockVelocity = v / numBoids;
-	
+		flockCenter = c / numBoids;
+		flockVelocity = v / numBoids;
+
 	}
 }
